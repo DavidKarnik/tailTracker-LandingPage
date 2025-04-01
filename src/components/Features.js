bdf95f5bfa9988a69bd21f6d9582223961ac3985
David Kárník <david.karnik@seznam.cz>
2025-04-01 21:23:40 +0200

    Frontend moore

--- src/components/Features.js
+++ src/components/Features.js
@@ -5,45 +5,45 @@ const Features = () => {
     const features = [
         {
             icon: '📍',
-            title: 'Real-Time GPS Tracking',
-            description: 'Never lose your dog again with live location updates.',
+            title: 'GPS poloha',
+            description: 'Nikdy neztraťte svého psa s GPS sledováním v reálném čase.',
             bgColor: '#4CAF50',
         },
         {
             icon: '📱',
-            title: 'App Notifications',
-            description: 'Alerts for activity, escape attempts, or low battery.',
-            bgColor: '#ff8732',
-        },
-        {
-            icon: '🏠',
-            title: 'Virtual Fence',
-            description: 'Create invisible boundaries and receive alerts when your dog crosses them.',
-            bgColor: '#4CAF50',
-        },
-        {
-            icon: '💓',
-            title: 'Health Monitoring',
-            description: 'Tracks heart rate, steps, and calories burned.',
+            title: 'Mobilní upozornění',
+            description: 'Upozornění na aktivitu, útěk, nebo třeba neobvyklé chování.',
             bgColor: '#ff8732',
         },
         {
             icon: '🔍',
-            title: 'Easy Identification',
-            description: 'Quickly identify your dog via QR code or by simply scanning with your mobile device.',
+            title: 'Identifikace',
+            description: 'Ryhlá identifikace psa kterýmkoliv chytrým mobilem. Stačí přiložit.',
+            bgColor: '#4CAF50',
+        },
+        {
+            icon: '💓',
+            title: 'Zdraví',
+            description: 'AI sleduje aktivitu, spánek, kroky, dýchání a další pro sestavení zdravotního profilu.',
+            bgColor: '#ff8732',
+        },
+        {
+            icon: '🏠',
+            title: 'Virtuální plot',
+            description: 'Vytvořte neviditelnou bezpečnou zónu a dostaňte upozornění při jejím překročení.',
             bgColor: '#4CAF50',
         },
         {
             icon: '🔔',
-            title: 'Bark Detection',
-            description: 'Analyzes barking patterns for better training.',
+            title: 'Offline režim',
+            description: 'Při ztrátě signálu se data neztratí. Po připojení se data znovu načtou.',
             bgColor: '#ff8732',
         },
     ];
 
     return (
         <section className={styles.featuresSection} id="features">
-            <h2 className={styles.sectionTitle}>Key Features</h2>
+            <h2 className={styles.sectionTitle}>Funkce</h2>
             <div className={styles.featuresContainer}>
                 {features.map((feature, index) => (
                     <FeatureCard key={index} {...feature} />
--- src/components/ImageTextSection.js
+++ src/components/ImageTextSection.js
@@ -4,15 +4,19 @@ export default function ImageTextSection() {
     return (
         <section className={styles.section}>
             <div className={styles.slopedImage}>
-                <img src="/images/dogB.jpg" alt="Happy Dog" />
+                <img src="/images/dogB.jpg" alt="Happy Dog"/>
             </div>
             <div className={styles.textContent}>
-                <h2>Designed for Adventure</h2>
-                <p>Waterproof, durable, and lightweight—perfect for active dogs.</p>
+                <h2>Dobrodružství má v krvi</h2>
+                {/*<p>Navržen na míru pro čtyřnohé parťáky.</p>*/}
                 <ul>
-                    <li>✅ 10-day battery life</li>
-                    <li>✅ Adjustable sizing</li>
-                    <li>✅ Chew-resistant material</li>
+                    <li>+ Vodotěsný a odolný - perfektní pro aktivní psy</li>
+                    <li>+ Zdravotní profil pomocí umělé inteligence</li>
+                    <li>+ Dosah signálu po celém světě</li>
+                    <li>+ Pro všechny velikosti obojků</li>
+                    <li>+ Predikce nemocí pomocí AI</li>
+                    <li>+ Dlouhá výdrž baterie</li>
+                    <li>+ Hlídá 24 hodin denně</li>
                 </ul>
             </div>
         </section>
